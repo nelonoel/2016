e45fd7bc27259a90e2bbfb1f659a5ea8be0c97bb
Aaron Noel De Leon <aaronnoeldeleon@Aarons-MacBook-Pro.local>
2016-05-04 16:50:22 +0800

    Basic usable version

---
+++ assets/js/components/banner.js
@@ -0,0 +1,12 @@
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+var video = document.querySelector('#landing-teaser');
+if(video) {
+	// video.width = x;
+	video.playbackRate = 0.45;
+}
